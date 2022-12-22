7c8e63fae015684ca2501fb31202257b3bde8fc4
Ifiok Udoidiok <ifiok@fleetrover.com>
2022-12-22 14:33:27 +0100

    Build Messages component

---
+++ src/pages/Chat/Messages.jsx
@@ -0,0 +1,64 @@
+import styles from './styles.module.css';
+import { useState, useEffect, useRef } from 'react';
+const Messages = ({ socket, messages, setMessages, messagesRecieved, setMessagesReceived }) => {
+
+
+    const messagesColumnRef = useRef(null); // Add this
+
+
+    // Runs whenever a socket event is recieved from the server
+    useEffect(() => {
+        socket.on('receive_message', (data) => {
+            console.log(data);
+            setMessagesReceived((state) => [
+                ...state,
+                {
+                    message: data.message,
+                    username: data.username,
+                    __createdtime__: data.__createdtime__,
+                },
+            ]);
+        });
+
+        // Remove event listener on component unmount
+        return () => socket.off('receive_message');
+    }, [socket]);
+
+
+    // dd/mm/yyyy, hh:mm:ss
+    function formatDateFromTimestamp(timestamp) {
+        const date = new Date(timestamp);
+        return date.toLocaleString();
+    }
+
+    return (
+        <div className={styles.messagesColumn}>
+            {messages.map((msg, i) => (
+                <div className={styles.message} key={i}>
+                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                        <span className={styles.msgMeta}>{msg.username}</span>
+                        <span className={styles.msgMeta}>
+                            {formatDateFromTimestamp(msg.__createdtime__)}
+                        </span>
+                    </div>
+                    <p className={styles.msgText}>{msg.message}</p>
+                    <br />
+                </div>
+            ))}
+            {messagesRecieved.map((msg, i) => (
+                <div className={styles.message} key={i}>
+                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                        <span className={styles.msgMeta}>{msg.username}</span>
+                        <span className={styles.msgMeta}>
+                            {formatDateFromTimestamp(msg.__createdtime__)}
+                        </span>
+                    </div>
+                    <p className={styles.msgText}>{msg.message}</p>
+                    <br />
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default Messages;
